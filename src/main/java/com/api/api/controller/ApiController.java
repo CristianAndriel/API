90cfbf696991839d1cd19648aa88262482ec4e65
CristianAndriel <cristianandrielsilva@gmail.com>
2026-02-14 22:16:44 -0300

    create: http get and post

--- src/main/java/com/api/api/controller/ApiController.java
+++ src/main/java/com/api/api/controller/ApiController.java
@@ -3,31 +3,22 @@ package com.api.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.api.api.ApiApplication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //Indica que estamos definindo uma API dentro desta classe
 public class ApiController {
 
-    private List<String> task = new ArrayList<>(); //Cria uma lista de tarefas vazia
-
-    private ObjectMapper objectMapper; //Cria um objeto ObjectMapper para converter objetos Java em JSON e vice-versa
-
-    public ApiController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper; //Injeta o ObjectMapper no construtor da classe
-    }
-
-
+    private final List<String> task = new ArrayList<>(); //Cria uma lista de tarefas vazia
 
     @GetMapping(path = "/tasks")//Indica que o método abaixo é um endpoint de GET e a rota para acessar esse endpoint é "/tasks"
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok(objectMapper.writeValueAsString(task)); //Retorna uma resposta HTTP 200 com o corpo "Hello, World!"
-
+    public ResponseEntity<List<String>> getTasks() {
+        return ResponseEntity.ok(task); //Retorna uma resposta HTTP 200 com a lista de tarefas em JSON
     }
 
-
-
+    @PostMapping(path = "/tasks")
+    public ResponseEntity<Void> createTask(@RequestBody String newTask) {
+        task.add(newTask);
+        return ResponseEntity.ok().build(); //Retorna uma resposta HTTP 200 sem corpo
+    }
 }
